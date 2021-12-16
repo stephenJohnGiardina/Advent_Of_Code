d38db1324bd014fb055a39d3d751bab3b7ceb776
stephenJohnGiardina <stephengiar@gmail.com>
2021-12-16 16:42:30 -0500

    Changed repo architecture

--- Day_1/day_1.py
+++ Day_1/day_1.py
@@ -1,10 +1,18 @@
-def obtain_sum_of_next_three(depths, index):
-    if index + 3 > len(depths):
-        return False
-    else:
-        return depths[index] + depths[index + 1] + depths[index + 2]
+def challenge_1():
+    with open("input.txt", "r+") as input:
+        greater_than_count = 0
+        previous = False
+        for line in input.readlines():
+            if previous == False:
+                previous = int(line)
+                continue
+            current = int(line)
+            if current > previous:
+                greater_than_count += 1
+            previous = current
+    print(greater_than_count)
 
-def main():
+def challenge_2():
     with open("input.txt", "r+") as input:
         depths = []
         for line in input.readlines():
@@ -25,5 +33,12 @@ def main():
 
     print(greater_than_count)
 
+def obtain_sum_of_next_three(depths, index):
+    if index + 3 > len(depths):
+        return False
+    else:
+        return depths[index] + depths[index + 1] + depths[index + 2]
+
 if __name__ == "__main__":
-    main()
+    challenge_1()
+    challenge_2()
