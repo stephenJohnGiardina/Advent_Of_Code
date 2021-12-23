9f80baf75b7e75eb2b017082b6ccf0046eac48b0
stephenJohnGiardina <stephengiar@gmail.com>
2021-12-23 16:54:07 -0500

    Day 13 complete

--- Day_13/day_13.py
+++ Day_13/day_13.py
@@ -1,8 +1,90 @@
 def challenge_1():
-    pass
+    coordinates, instructions = load_paper()
+    coordinates = fold_vertically(coordinates, instructions[0][1])
+
+    print(len(coordinates))
+
+def load_paper():
+    coordinates = []
+    instructions = []
+    all_coordinates_found = False
+    with open("Day_13/input.txt", "r+") as input:
+        for line in input.readlines():
+            if line.strip() == "":
+                all_coordinates_found = True
+                continue
+            if not all_coordinates_found:
+                coordinates.append(tuple(map(lambda x: int(x), line.strip().split(","))))
+            else:
+                instruction = line.strip().split("=")
+                direction = instruction[0][-1]
+                location = int(instruction[1])
+                instructions.append((direction, location))
+
+    return coordinates, instructions
+
+def fold_vertically(coordinates, location):
+    new_coordinates = []
+
+    for coordinate in coordinates:
+        if coordinate[0] > location:
+            new_x = location - (coordinate[0] - location)
+            new_y = coordinate[1]
+            new_coordinate = (new_x, new_y)
+            new_coordinates.append(new_coordinate)
+        else:
+            new_coordinates.append(coordinate)
+    
+    return list(set(new_coordinates))
 
 def challenge_2():
-    pass
+    coordinates, instructions = load_paper()
+
+    for instruction in instructions:
+        if instruction[0] == "x":
+            coordinates = fold_vertically(coordinates, instruction[1])
+        if instruction[0] == "y":
+            coordinates = fold_horizontally(coordinates, instruction[1])
+
+    print_paper(coordinates)
+
+def fold_horizontally(coordinates, location):
+    new_coordinates = []
+
+    for coordinate in coordinates:
+        if coordinate[1] > location:
+            new_x = coordinate[0]
+            new_y = location - (coordinate[1] - location)
+            new_coordinate = (new_x, new_y)
+            new_coordinates.append(new_coordinate)
+        else:
+            new_coordinates.append(coordinate)
+    
+    return list(set(new_coordinates))
+
+def print_paper(coordinates):
+    max_x = 0
+    max_y = 0
+
+    paper = []
+
+    for coordinate in coordinates:
+        if coordinate[0] > max_x:
+            max_x = coordinate[0]
+        if coordinate[1] > max_y:
+            max_y = coordinate[1]
+
+    for row in range(max_y + 1):
+        paper_row = []
+        for col in range(max_x + 1):
+            if (col, row) in coordinates:
+                paper_row.append("#")
+            else:
+                paper_row.append(" ")
+        paper.append("".join(paper_row))
+    
+    for line in paper:
+        print(line)
 
 if __name__ == "__main__":
     challenge_1()
--- Day_9/day_9.py
+++ Day_9/day_9.py
@@ -12,7 +12,7 @@ def challenge_1():
 def load_height_map():
     height_map = []
 
-    with open("Day_9/input1.txt", "r+") as input:
+    with open("Day_9/input.txt", "r+") as input:
         for line in input.readlines():
 
             height_map.append(list(map(lambda x: int(x), line.strip())))
@@ -53,43 +53,37 @@ def challenge_2():
     for low_point in low_points:
         basin_sizes.append(find_size_of_basin(low_point, height_map))
 
-    print(basin_sizes)
     basin_sizes.sort()
-    print(basin_sizes)
-    print(basin_sizes[-3], basin_sizes[-2], basin_sizes[-1])
+
     print(basin_sizes[-3] * basin_sizes[-2] * basin_sizes[-1])
 
 def find_size_of_basin(low_point, height_map):
-    adjacent_points = obtain_adjacent_points(low_point, height_map)
+    adjacent_points = []
 
-    basin_points = set(adjacent_points)
+    obtain_adjacent_points(low_point, height_map, adjacent_points)
 
-    return len(basin_points)
+    return len(adjacent_points)
 
-def obtain_adjacent_points(low_point, height_map):
-    adjacent_points = [low_point]
+def obtain_adjacent_points(low_point, height_map, adjacent_points):
+    if low_point in adjacent_points:
+        return
+    adjacent_points.append(low_point)
 
     row = low_point[0]
     col = low_point[1]
 
-    point = height_map[row][col]
-    if point == 8:
-        return adjacent_points
-
     if row > 0:
-        if point + 1 == height_map[row - 1][col]:
-            adjacent_points += obtain_adjacent_points((row - 1, col), height_map)
+        if height_map[row - 1][col] != 9:
+            obtain_adjacent_points((row - 1, col), height_map, adjacent_points)
     if row < len(height_map) - 1:
-        if point + 1 == height_map[row + 1][col]:
-            adjacent_points += obtain_adjacent_points((row + 1, col), height_map)
+        if height_map[row + 1][col] != 9:
+            obtain_adjacent_points((row + 1, col), height_map, adjacent_points)
     if col > 0:
-        if point + 1 == height_map[row][col - 1]:
-            adjacent_points += obtain_adjacent_points((row, col - 1), height_map)
+        if height_map[row][col - 1] != 9:
+            obtain_adjacent_points((row, col - 1), height_map, adjacent_points)
     if col < len(height_map[row]) - 1:
-        if point + 1 == height_map[row][col + 1]:
-            adjacent_points += obtain_adjacent_points((row, col + 1), height_map)
-
-    return adjacent_points
+        if height_map[row][col + 1] != 9:
+            obtain_adjacent_points((row, col + 1), height_map, adjacent_points)
 
 if __name__ == "__main__":
     challenge_1()
