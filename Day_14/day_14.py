2a956377f20f1ac1e7ca6b1dbaeed39c13f9db64
stephenJohnGiardina <stephengiar@gmail.com>
2022-01-26 17:36:31 -0500

    Day 14 complete

--- Day_14/day_14.py
+++ Day_14/day_14.py
@@ -4,10 +4,11 @@ from datetime import datetime
 def challenge_1():
     polymer_template, pair_insertion = load_polymer_data()
 
-    for step in range(10):
-        polymer_template = insert_into_pairs(polymer_template, pair_insertion)
+    polymer_template = insert_elements(polymer_template, pair_insertion, 10)
 
-    amounts_of_each_element = get_amounts_of_each_element(polymer_template)
+    amounts_of_each_element_object = get_amounts_of_each_element_object(polymer_template)
+
+    amounts_of_each_element = get_amounts_of_each_element(amounts_of_each_element_object)
 
     print(max(amounts_of_each_element) - min(amounts_of_each_element))
 
@@ -28,6 +29,11 @@ def load_polymer_data():
 
     return polymer_template, pair_insertion
 
+def insert_elements(polymer_template, pair_insertion, number_of_steps):
+    for step in range(number_of_steps):
+        polymer_template = insert_into_pairs(polymer_template, pair_insertion)
+    return polymer_template
+
 def insert_into_pairs(polymer_template, pair_insertion):
     new_polymer_template = ""
     first = True
@@ -41,7 +47,7 @@ def insert_into_pairs(polymer_template, pair_insertion):
 
     return new_polymer_template
 
-def get_amounts_of_each_element(polymer_template):
+def get_amounts_of_each_element_object(polymer_template):
     element_data = {}
 
     for element in polymer_template:
@@ -50,15 +56,41 @@ def get_amounts_of_each_element(polymer_template):
         else:
             element_data[element] += 1
     
+    return element_data
+
+def get_amounts_of_each_element(element_data):
     element_amounts = []
 
     for amount in element_data.values():
         element_amounts.append(amount)
-    
+
     return element_amounts
 
 def challenge_2():
-    pass
+    polymer_template, pair_insertion = load_polymer_data()
+    pair_data = {}
+
+    for pair in pair_insertion.keys():
+        pair_data[pair] = get_amounts_of_each_element_object(insert_elements(pair, pair_insertion, 20))
+        pair_data[pair][pair[1]] -= 1
+
+    polymer_template = insert_elements(polymer_template, pair_insertion, 20)
+
+    amounts_of_each_element_object = {}
+
+    for element_index in range(len(polymer_template) - 1):
+        pair = polymer_template[element_index:element_index + 2]
+        for element in pair_data[pair].keys():
+            if element not in amounts_of_each_element_object:
+                amounts_of_each_element_object[element] = pair_data[pair][element]
+            else:
+                amounts_of_each_element_object[element] += pair_data[pair][element]
+    
+    amounts_of_each_element_object[polymer_template[-1]] += 1
+
+    amounts_of_each_element = get_amounts_of_each_element(amounts_of_each_element_object)
+
+    print(max(amounts_of_each_element) - min(amounts_of_each_element))
 
 if __name__ == "__main__":
     print("Starting Challenges for Day 14")
