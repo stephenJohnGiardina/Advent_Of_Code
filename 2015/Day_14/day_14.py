315ae2f9e88afa50ff0dcf76098bf7a076b2837f
stephenJohnGiardina <stephengiar@gmail.com>
2022-02-22 15:37:15 -0500

    2015 Day 14 and 15 complete

--- 2015/Day_14/day_14.py
+++ 2015/Day_14/day_14.py
@@ -3,14 +3,76 @@ from datetime import datetime
 
 
 def challenge_1():
-    pass
+    all_reindeer = obtain_all_reindeer()
+    for reindeer in all_reindeer:
+        reindeer.travel_distance(2503)
+    fastest_reindeer = max(all_reindeer, key = lambda reindeer: reindeer.current_distance)
+    print(
+        "The reindeer that traveled the furthest after 2053 seconds was",
+        fastest_reindeer.name,
+        "with a distance of",
+        fastest_reindeer.current_distance
+        )
 
-def obtain_input():
+class Reindeer:
+    def __init__(self, name, speed, endurance, rest_required):
+        self.name = name
+        self.speed = speed
+        self.endurance = endurance
+        self.rest_required = rest_required
+        self.traveling = True
+        self.current_endurance = endurance
+        self.current_rest_required = 0
+        self.current_distance = 0
+        self.points = 0
+
+    def travel_distance(self, time_traveled):
+        for second in range(time_traveled):
+            if self.traveling:
+                self.current_distance += self.speed
+                self.current_endurance -= 1
+                if self.current_endurance == 0:
+                    self.current_rest_required = self.rest_required
+                    self.traveling = False
+            else:
+                self.current_rest_required -= 1
+                if self.current_rest_required == 0:
+                    self.current_endurance = self.endurance
+                    self.traveling = True
+
+def obtain_all_reindeer():
+    all_reindeer = []
     with open(os.path.join("2015", "Day_14", "input.txt"), "r+") as input:
-        pass
+        for line in input.readlines():
+            name = line.split(" ")[0]
+            speed = int(line.split(" ")[3])
+            endurance = int(line.split(" ")[6])
+            rest_required = int(line.split(" ")[-2])
+            all_reindeer.append(Reindeer(name, speed, endurance, rest_required))
+    return all_reindeer
 
 def challenge_2():
-    pass
+    all_reindeer = obtain_all_reindeer()
+    for second in range(2503):
+        for reindeer in all_reindeer:
+            reindeer.travel_distance(1)
+        for winning_reindeer in obtain_winning_reindeer(all_reindeer):
+            winning_reindeer.points += 1
+    winning_reindeer = max(all_reindeer, key = lambda reindeer: reindeer.points)
+    print(
+        "The reindeer that obtained the most points after 2053 seconds was",
+        winning_reindeer.name,
+        "with a score of",
+        winning_reindeer.points
+        )
+
+def obtain_winning_reindeer(all_reindeer):
+    winning_reindeer = []
+    max_distance = max(all_reindeer, key = lambda reindeer: reindeer.current_distance).current_distance
+    for reindeer in all_reindeer:
+        if reindeer.current_distance == max_distance:
+            winning_reindeer.append(reindeer)
+    return winning_reindeer
 
 if __name__ == "__main__":
     print("Starting Challenges for Year 2015 Day 14")
--- 2015/Day_15/day_15.py
+++ 2015/Day_15/day_15.py
@@ -3,14 +3,103 @@ from datetime import datetime
 
 
 def challenge_1():
-    pass
+    ingredients = obtain_ingredients()
+    total_score = 0
+    all_quantities = obtain_all_quantities(0)
+    for quantities in all_quantities:
+        ingredients_and_quantities = (
+            (ingredients[0], quantities[0]),
+            (ingredients[1], quantities[1]),
+            (ingredients[2], quantities[2]),
+            (ingredients[3], quantities[3]),
+            )
+        current_score = obtain_total_score_of_cookie(ingredients_and_quantities)
+        if current_score > total_score:
+            total_score = current_score
+    print("The total score of the cookie with the highest possible total score =", total_score)
 
-def obtain_input():
+class Ingredient:
+    def __init__(self, name, capacity, durability, flavor, texture, calories):
+        self.name = name
+        self.capacity = capacity
+        self.durability = durability
+        self.flavor = flavor
+        self.texture = texture
+        self.calories = calories
+
+def obtain_ingredients():
+    ingredients = []
     with open(os.path.join("2015", "Day_15", "input.txt"), "r+") as input:
-        pass
+        for line in input.readlines():
+            name = line.split(" ")[0][:-1]
+            capacity = int(line.split(" ")[2][:-1])
+            durability = int(line.split(" ")[4][:-1])
+            flavor = int(line.split(" ")[6][:-1])
+            texture = int(line.split(" ")[8][:-1])
+            calories = int(line.split(" ")[10])
+            ingredients.append(Ingredient(name, capacity, durability, flavor, texture, calories))
+    return ingredients
+
+def obtain_all_quantities(current_sum, current_quantities=[]):
+    if len(current_quantities) == 4:
+        if sum(current_quantities) == 100:
+            return [current_quantities]
+        return []
+    quantities = []
+    for quantity in range(1, 100):
+        if current_sum + quantity > 100:
+            break
+        quantities += obtain_all_quantities(current_sum + quantity, current_quantities + [quantity])
+    return quantities
+
+def obtain_total_score_of_cookie(ingredients_and_quantities):
+    capacity = 0
+    durability = 0
+    flavor = 0
+    texture = 0
+    for ingredient, quantity in ingredients_and_quantities:
+        capacity += quantity * ingredient.capacity
+        durability += quantity * ingredient.durability
+        flavor += quantity * ingredient.flavor
+        texture += quantity * ingredient.texture
+    if capacity < 0:
+        capacity = 0
+    if durability < 0:
+        durability = 0
+    if flavor < 0:
+        flavor = 0
+    if texture < 0:
+        texture = 0
+    total_score = capacity * durability * flavor * texture
+    if total_score < 0:
+        return 0
+    return total_score
 
 def challenge_2():
-    pass
+    ingredients = obtain_ingredients()
+    total_score = 0
+    all_quantities = obtain_all_quantities(0)
+    for quantities in all_quantities:
+        ingredients_and_quantities = (
+            (ingredients[0], quantities[0]),
+            (ingredients[1], quantities[1]),
+            (ingredients[2], quantities[2]),
+            (ingredients[3], quantities[3]),
+            )
+        current_score = obtain_total_score_of_cookie(ingredients_and_quantities)
+        if obtain_calories_of_cookie(ingredients_and_quantities) == 500:
+            if current_score > total_score:
+                total_score = current_score
+    print(
+        "Of the cookies that are 500 calories, the total score of the cookie with the highest possible total score =",
+        total_score
+        )
+
+def obtain_calories_of_cookie(ingredients_and_quantities):
+    total_calories = 0
+    for ingredient, quantity in ingredients_and_quantities:
+        total_calories += quantity * ingredient.calories
+    return total_calories
 
 if __name__ == "__main__":
     print("Starting Challenges for Year 2015 Day 15")
