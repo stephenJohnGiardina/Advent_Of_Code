0b90e48a2d3521141f813c2ce17af7df3f5a957c
stephenJohnGiardina <stephengiar@gmail.com>
2022-09-29 11:35:55 -0400

    2015 Day 17 complete

--- 2015/Day_17/day_17.py
+++ 2015/Day_17/day_17.py
@@ -3,24 +3,50 @@ from datetime import datetime
 
 
 def challenge_1():
-    pass
+    containers = obtain_containers()
+    possible_containers = obtain_possible_containers(containers)
+    unique_possible_containers = list(set(map(lambda x: tuple(sorted(x)), possible_containers)))
+    number_of_possible_containers = len(unique_possible_containers)
+    print("Number of different combinations of containers that can exactly fit all 150 liters of eggnog =", number_of_possible_containers)
+    return unique_possible_containers
 
-def obtain_input():
+def obtain_containers():
+    containers = []
     with open(os.path.join("2015", "Day_17", "input.txt"), "r+") as input:
-        pass
+        for line_number, line in enumerate(input.readlines()):
+            containers.append((int(line), line_number))
+    return containers
 
-def challenge_2():
-    pass
+def obtain_possible_containers(containers, current_containers=[]):
+    if sum(list(map(lambda x: x[0], current_containers))) == 150:
+        return [current_containers]
+    possible_containers = []
+    for container_index, container in enumerate(containers):
+        if sum(list(map(lambda x: x[0], current_containers))) + container[0] <= 150:
+            new_containers = containers[:container_index] + containers[container_index + 1:]
+            possible_containers += obtain_possible_containers(new_containers, current_containers + [container])
+    return possible_containers
+
+def challenge_2(unique_possible_containers):
+    minimum_number_of_containers = float("inf")
+    number_of_minimum_number_of_containers = 1
+    for unique_possible_container in unique_possible_containers:
+        if len(unique_possible_container) < minimum_number_of_containers:
+            number_of_minimum_number_of_containers = 1
+            minimum_number_of_containers = len(unique_possible_container)
+        elif len(unique_possible_container) == minimum_number_of_containers:
+            number_of_minimum_number_of_containers += 1
+    print("Number of different ways to fit exactly 150 liters of eggnog using the minimum number of containers =", number_of_minimum_number_of_containers)
 
 if __name__ == "__main__":
     print("Starting Challenges for Year 2015 Day 17")
 
     print("Starting Challenge 1")
     challenge_1_start_time = datetime.now()
-    challenge_1()
+    unique_possible_containers = challenge_1()
     print("Challenge 1 complete in " + str(datetime.now() - challenge_1_start_time))
 
     print("Starting Challenge 2")
     challenge_2_start_time = datetime.now()
-    challenge_2()
+    challenge_2(unique_possible_containers)
     print("Challenge 2 complete in " + str(datetime.now() - challenge_2_start_time))
